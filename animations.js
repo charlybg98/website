261471a41617b87d45d14042562487b2597c7ae2
Carlos Bustamante <contact@charlyfive.com>
2023-08-14 18:47:53 -0600

    New design and organization

--- animations.js
+++ animations.js
@@ -1,10 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const mainContent = document.querySelector(".main-content");
-
-    function fadeInContent() {
-        mainContent.classList.add("visible");
-    }
-
-    // Aplicar el efecto fade in después de un pequeño retraso
-    setTimeout(fadeInContent, 500);
+$(document).ready(function () {
+    $(".portfolio-section, .description-section, .projects-section, .details-section").css("opacity", "1");
 });
