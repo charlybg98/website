20644241de89874e916b0207c5be759cadef1348
Carlos Bustamante <contact@charlyfive.com>
2024-07-26 17:53:33 -0600

    Added files

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,22 @@ navLinks.forEach((item) => {
         console.log(item.getAttribute('href'))
     })
 })
+
+document.querySelector('#sidebar .toggle-sidebar').addEventListener('click', function () {
+    document.querySelector('#sidebar').classList.toggle('open')
+})
+
+var options = {
+    strings: ['Data Scientist', 'Machine Learning Enthusiast'],
+    loop: true,
+    typeSpeed: 80,
+    backSpeed: 10
+};
+
+new Typed('.field h2', options)
+
+for (let i = 1; i <= 15; i++) {
+    let meteor = document.createElement('span');
+    meteor.classList = 'meteor'
+    document.querySelector('#home .meteor-shower').append(meteor);
+}
