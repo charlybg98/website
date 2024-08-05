adf08117e2c6b3e6da66299dea4b2ca1e5ef36b3
Carlos Bustamante <contact@charlyfive.com>
2024-08-04 19:12:34 -0600

    Added and changed files

--- assets/js/script_es.js
+++ assets/js/script_es.js
@@ -14,7 +14,7 @@ document.querySelector('#sidebar .toggle-sidebar').addEventListener('click', fun
 })
 
 var options = {
-    strings: ['Data Scientist', 'Machine Learning Enthusiast', 'AI Developer'],
+    strings: ['Científico de Datos', 'Apasionado por el Aprendizaje Automático', 'Desarrollador de IA'],
     loop: true,
     typeSpeed: 80,
     backSpeed: 10
